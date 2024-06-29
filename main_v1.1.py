38e65d27b8d19a5f0a9f9630c02252df6cc0e5f1
mirkojp <mirkopavan88878@gmail.com>
2024-06-29 17:55:06 -0300

    final

--- main_v1.1.py
+++ main_v1.1.py
@@ -1,7 +1,14 @@
-def is_alpha(char):
+def is_alpha_lower(char):
     if not char:
         return False
-    if ("A" <= char <= "Z") or ("a" <= char <= "z"):#This compares ASCII values
+    if "a" <= char <= "z":#This compares ASCII values
+        return True
+    return False
+
+def is_alpha_upper(char):
+    if not char:
+        return False
+    if "A" <= char <= "Z":#This compares ASCII values
         return True
     return False
 
@@ -13,38 +20,52 @@ def is_digit(char):
     return False
 
 def is_aldigit(char):
-    if is_alpha(char) and is_digit(char):
+    if is_alpha_lower(char) or is_alpha_upper(char):
         return True
-    else:
-        return False
-    
-def is_space(char):
-    if char == " ":
+    elif is_alpha_upper(char):
+        return True
+    elif is_digit(char):
         return True
     else:
         return False
 
+
+def isspace(s):
+    for char in s:
+        # print(char)
+        if char not in (" ", "\n", "\r"): # each line end with \r\n, carriage return(moves cursor to end of line) + newline
+            return False
+    return True
 
 
 def is_identifier(token: str) -> bool:
     # Constants
     Q0 = 0
-    F = [1]
+    F = [2]
     Q = range(3)
-    SIGMA = {"LETTER": 0, "DIGIT": 1,"UNDERSCORE":2, "OTHER": 3} #Letter includes aA-zZ
+
+    SIGMA = {
+        "LETTER_UPPER": 0,
+        "LETTER_LOWER": 1,
+        "UNDERSCORE": 2,
+        "DIGIT": 3,
+        "OTHER": 4,
+    }
 
     # Transition table
     DELTA = [
-        [1, 1, 1, 2], # Transitions from state 0
-        [1, 1, 1, 2],  # Transitions from state 1
-        [2, 2, 2, 2],  # Transitions from state 2 (trap state)
+        [2, 2, 2, 1, 1], # Transitions from state 0
+        [1, 1, 1, 1, 1],  # Transitions from state 1 (trap state)
+        [2, 2, 2, 2, 1],  # Transitions from state 2 
     ]
 
     # Evaluate Char
     def carasimbolo(char: str) -> int:
-        if char.isalpha(): # .if char in [0..9] might be replaced by custom
-            return SIGMA["LETTER"]
-        elif char.isdigit(): # if char in [A..z] might be replaced by custom
+        if is_alpha_upper(char): 
+            return SIGMA["LETTER_UPPER"]
+        if is_alpha_lower(char): 
+            return SIGMA["LETTER_LOWER"]
+        elif is_digit(char): 
             return SIGMA["DIGIT"]
         elif char == "_":
             return SIGMA["UNDERSCORE"]
@@ -54,7 +75,7 @@ def is_identifier(token: str) -> bool:
     estado_actual = Q0
     i1 = 0
 
-    while i1 < len(token) and estado_actual != 2:
+    while i1 < len(token) and estado_actual != 1:
         estado_actual = DELTA[estado_actual][carasimbolo(token[i1])]
         i1 += 1
 
@@ -83,7 +104,7 @@ def is_integer(token : str)-> bool:
     # Evaluate Char
 
     def carasimbolo(char: str) -> int:
-        if char.isdigit():  # if char in [A..z] might be replaced by custom
+        if is_digit(char):  # if char in [A..z] might be replaced by custom
             return SIGMA["DIGIT"]
         elif char == "-":
             return SIGMA["MINUS"]
@@ -128,7 +149,7 @@ def is_real(token : str)->bool:
     ]
 
     def carasimbolo(char: str) -> int:
-        if char.isdigit():
+        if is_digit(char):
             return SIGMA["DIGIT"]
         elif char == "-":
             return SIGMA["MINUS"]
@@ -183,12 +204,13 @@ class LexicalAnalyzer:
         token = ""
         while True:
             car = self.read_car()
-            if car == self.FinArch or car.isspace():
+            # print(car)
+            if car == self.FinArch or isspace(car):
                 if token:
                     return token
                 if car == self.FinArch:
                     return None
-            elif not car.isalnum() and car not in {"_","."}:
+            elif not is_aldigit(car) and car not in {"_","."}:
                 if token:
                     self.control -= 1  # Put back the non-alphanumeric character for next token rea)
                     return token
